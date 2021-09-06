d1897be12b27d94f3c2a913da5151c55bee3959e
Liss uwu <87399759+Liss0exe@users.noreply.github.com>
2021-09-06 18:51:23 -0500

    Update api.js

--- Js/api.js
+++ Js/api.js
@@ -4,7 +4,7 @@ alert('Bienvenido, ' + pokemon);
 const pokeCard = document.querySelector('[data-poke-card]');
 const pokeName = document.querySelector('[data-poke-name]');
 const pokeImg = document.querySelector('[data-poke-img]');
-const pokeImgContainer = document.querySelector('[data-poke-img-containes]');
+const pokeImgContainer = document.querySelector('[data-poke-img-container]');
 const pokeId = document.querySelector('[data-poke-id]');
 const pokeTypes = document.querySelector('[data-poke-types]');
 const pokeStats = document.querySelector('[data-poke-stats]');
